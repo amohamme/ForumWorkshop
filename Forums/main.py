e3496e09538716c2673b301571171082731b1852
Unknown <a.h.moslem@gmail.com>
2018-02-25 20:03:18 +0200

    Updated the import and class instants

--- Forums/main.py
+++ Forums/main.py
@@ -2,8 +2,7 @@
 # Main
 ########################################
 
-from models import member
-from models import post
+import models
 
 name1 = 'Ahmed'
 age1  = 32
@@ -19,12 +18,12 @@ content2 = 'content 2'
 title3 = 'title 3'
 content3 = 'content3'
 
-member1 = member(name1,age1)
-member2 = member(name2,age2)
+member1 = models.member(name1,age1)
+member2 = models.member(name2,age2)
 
-post1 = post(title1,content1)
-post2 = post(title2,content2)
-post3 = post(title3,content3)
+post1 = models.post(title1,content1)
+post2 = models.post(title2,content2)
+post3 = models.post(title3,content3)
 
 print(member1.name)
 print(member2.age)
