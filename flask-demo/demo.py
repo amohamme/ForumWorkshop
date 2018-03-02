0a5c46f16e581e9fd588533211b789cd183565c3
Unknown <a.h.moslem@gmail.com>
2018-03-02 19:16:28 +0200

    Flask Demo
    Added say_hello function

---
+++ flask-demo/demo.py
@@ -0,0 +1,19 @@
+########################################
+# flask demo
+########################################
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def home():
+     return "Hi there"
+
+@app.route('/SayHello/<username>')
+def say_hello(username):
+    return 'Hello %s' % username
+
+app.run()
+
