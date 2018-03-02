ce25020a74c80ad2e970422c67a0021bd9966a4e
Unknown <a.h.moslem@gmail.com>
2018-03-02 21:36:39 +0200

    Jinja
    Added CSS file
    Added Post store to index.html

--- Forums/app.py
+++ Forums/app.py
@@ -3,13 +3,20 @@
 ########################################
 
 from flask import Flask, render_template
+from Forums import models
+from Forums import stores
 
 app = Flask(__name__)
+
+post_store = stores.PostStore()
+post_store.add(models.post("Life", "Life is alawys great"))
+post_store.add(models.post("Sunshine", "Sunshine is amazing"))
+
 
 @app.route("/")
 @app.route("/index")
 def home():
-     return render_template("index.html")
+     return render_template("index.html", posts = post_store.get_all())
 
 @app.route('/SayHello/<username>')
 def say_hello(username):
